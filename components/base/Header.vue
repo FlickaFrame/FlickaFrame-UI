ac0ed18c454919313bd53f37ef355f95c5bc2aed
Shadowsight9 <huangzheng@shu.edu.cn>
2023-10-24 16:00:14 +0800

    feat: add header

--- components/base/Header.vue
+++ components/base/Header.vue
@@ -5,8 +5,24 @@ import Logo from '~/assets/logo.png'
 
 <template>
 
-  <nav class="bg-blue flex h-20 w-full">
-    <img :src="Logo" class="w-80">
+  <nav class="border-b border-slate flex gap-2 fixed h-20 w-full justify-between items-center top-0">
+    <img :src="Logo" alt="logo" class="w-80">
+    <input
+      type="text" class="border border-blue rounded shadow flex-1 h-10 max-w-1/2"
+      placeholder="这是搜索框"
+    >
+    <div>
+      这是头像 个人信息
+    </div>
   </nav>
 
 </template>
+
+<style scoped>
+nav {
+  background-image: radial-gradient(transparent 1px, white, 1px);
+  background-size: 4px 4px;
+  backdrop-filter: saturate(50%) blur(4px);
+}
+
+</style>
