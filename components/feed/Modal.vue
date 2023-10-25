dd65e465c5502f5ef18520c40473216eb9870e67
Shadowsight9 <huangzheng@shu.edu.cn>
2023-10-25 17:21:26 +0800

    feat: add icons

--- components/feed/Modal.vue
+++ components/feed/Modal.vue
@@ -10,10 +10,8 @@ watch(() => show.value, (val) => {
   }
 })
 
-const { escape } = useMagicKeys()
-
-watch(escape, (v) => {
-  if (v) { show.value = false }
+onKeyStroke('esc', () => {
+  show.value = false
 })
 </script>
 
@@ -22,11 +20,20 @@ watch(escape, (v) => {
 
     <div
       v-if="show"
-      class="flex fixed h-screen w-screen justify-center items-center left-0 top-0 z-100 backdrop-grayscale"
+      class="bg-black bg-opacity-10 flex fixed h-screen w-screen justify-center items-center left-0 top-0 z-100 transition-all"
       @click="show = !show"
     >
-      <Video class="bg-blue flex flex-row h-192 w-108 justify-center" />
-      <div class="bg-white flex flex-row h-screen w-100 justify-center">评论区</div>
+
+      <div class="bg-white border-2 rounded-full shadow flex fixed h-10 w-10 justify-center items-center left-10 top-10 transition-all hover:border-slate-300 hover:shadow-lg">
+        <div class="i-mdi-close-thick text-6 text-slate-600" />
+      </div>
+
+      <div class="my-10 rounded-8 shadow-lg flex h-9/10 w-7/10 overflow-hidden" @click.stop>
+        <div class="left h-full w-6/10">
+          <Video />
+        </div>
+        <div class="left bg-gray flex-1 h-full" />
+      </div>
     </div>
 
   </Teleport>
--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig, presetMini, presetUno } from 'unocss'
+import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
   presets: [
     presetMini(),
     presetUno(),
+    presetIcons({
+      collections: {
+        mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
+      },
+    }),
   ],
 })
