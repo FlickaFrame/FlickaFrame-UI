a1941b00b20cae06af689753c9cbfee139197d34
Shadowsight9 <huangzheng@shu.edu.cn>
2023-11-06 00:07:23 +0800

    feat: 增加无限加载

--- composables/useFeedData.ts
+++ composables/useFeedData.ts
@@ -9,9 +9,9 @@ export function useFeedData(categoryId: Ref<number>, limit = 20) {
   const cursor = computed(() => cursorRecord.value[categoryId.value])
 
   const isEndRecord = ref<Record<number, boolean>>({})
-  const isEnd = computed(() => isEndRecord.value[categoryId.value])
+  const isEnd = computed(() => Boolean(isEndRecord.value[categoryId.value]))
 
-  const { refresh, pending } = useAsyncData(async () => {
+  const { refresh: addMore, pending } = useAsyncData(async () => {
     const currentId = categoryId.value
     const { success, data, msg } = await getVideoFeed({
       cursor: cursor.value,
@@ -33,5 +33,5 @@ export function useFeedData(categoryId: Ref<number>, limit = 20) {
     feedListRecord.value[currentId].push(...data.list)
   }, { watch: [categoryId], immediate: true })
 
-  return { feedList, isEnd, refresh, pending }
+  return { feedList, isEnd, addMore, pending }
 }
