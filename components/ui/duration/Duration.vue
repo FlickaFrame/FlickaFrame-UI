e05afe7dd0c39b003acc7fcd75dd159575a9d150
Shadowsight9 <huangzheng@shu.edu.cn>
2023-11-07 23:07:48 +0800

    fix: err fix

--- components/ui/duration/Duration.vue
+++ components/ui/duration/Duration.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
+defineProps<{
+  value: number
+}>()
+
 function formatDuration(duration: number) {
   const min = String(Math.floor(duration / 60)).padStart(2, '0')
   const sec = String(Math.floor(duration % 60)).padStart(2, '0')
   return `${min}:${sec}`
 }
-
- defineProps<{
-   value: number
- }>
 
 </script>
 
